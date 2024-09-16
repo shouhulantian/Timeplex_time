f9d704de2cbc89a8b673571954302aebac1654eb
shouhulantian <237448659@qq.com>
2024-09-16 11:01:04 +0200

    time prediction add tmrr

--- evaluate.py
+++ evaluate.py
@@ -943,7 +943,7 @@ def get_tmrr_ranking(
         for j in range(len(test_target)):
             t_score = scores[j].clone().detach()
             t_score[target] = -1e6
-            target_score = scores[j][test_target[i]]
+            target_score = scores[j][test_target[j]]
             rank_triple = torch.sum((t_score > target_score).float()).cpu().item() + 1
             positive_rank.append(rank_triple)
         positive_mrr = numpy.mean([1/i for i in positive_rank])
